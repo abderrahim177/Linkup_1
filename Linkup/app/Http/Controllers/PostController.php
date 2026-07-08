6fb4a6a6230dfee21c4d221c4a28456b80d94d27
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-07-08 16:53:29 +0100

    add function of show count of posts

--- Linkup/app/Http/Controllers/PostController.php
+++ Linkup/app/Http/Controllers/PostController.php
@@ -52,7 +52,7 @@ class PostController extends Controller
         $post->delete();
         return redirect()->route('dashboard')->with('success', 'Deleted with success !');
     }
-    
+
     public function show(Post $post){
         return view('details',compact('post'));
     }
